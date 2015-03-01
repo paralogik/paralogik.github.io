04ece701da112822ff2d3a00c8e27c4bac499dbf
Nick Levantis <niklevantis@gmail.com>
2015-03-01 03:12:52 +0200

    new

---
+++ js/particle.js
@@ -0,0 +1,14 @@
+/**
+ * Particleground demo
+ * @author Jonathan Nicol - @mrjnicol
+ */
+
+$(document).ready(function() {
+  $('body').particleground({
+    dotColor: '#7F7F7F',
+    lineColor: '#fff',
+    lineWidth: '0.1111',
+    particleRadius: '0',
+    parallaxMultiplier: '5'
+  });
+});
